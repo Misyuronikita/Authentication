4d593f0b27696bd20763a54a6d1fdb1dba143356
unknown <fixik2019@gmail.com>
2023-03-29 00:57:26 +0300

    save

--- index.php
+++ index.php
@@ -5,22 +5,49 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
+<h1>My project</h1><br>
+<h2>Registration</h2>
 <form action="register.php" method="post">
-    <input type="text" placeholder="Login" name="login">
-    <input type="text" placeholder="Password" name="password">
-    <input type="text" placeholder="Repeat Password" name="repeatPassword">
-    <input type="text" placeholder="Email" name="email">
+    <div class="form-group">
+        <label for="ExampleInputLogin"> Your Login
+            <input type="text" class="form-control" placeholder="Login" name="login">
+        </label>
+    </div>
+    <div class="form-group">
+        <label for="ExampleInputPassword1"> Input your password
+            <input type="text" class="form-control" placeholder="Password" name="password">
+        </label>
+    </div>
+    <div class="form-group">
+        <label for="ExampleInputPassword2"> Confirm your password
+        <input type="text" class="form-control" placeholder="Repeat Password" name="repeatPassword">
+        </label>
+    </div>
+    <div class="form-group">
+        <label for="ExampleInputEmail"> Input your email
+        <input type="text" class="form-control" aria-describedby="emailHelp" placeholder="Email" name="email">
+        </label>
+    </div>
     <button type="submit">Sign Up</button>
 </form>
 
+<br><h2>Authorisation</h2>
 <form action="login.php" method="post">
-    <input type="text" placeholder="Login" name="login">
-    <input type="text" placeholder="Password" name="password">
+    <div class="form-group">
+    <label> Input your login
+    <input type="text" class="form-control" placeholder="Login" name="login">
+    </label>
+    </div>
+        <div class="form-group">
+    <label> Input your password
+    <input type="text" class="form-control" placeholder="Password" name="password">
+    </label>
+        </div>
     <button type="submit">Sign In</button>
 </form>
-
 </body>
 </html>
