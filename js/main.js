903f1200e17ecfd068bbed10330f73b541695684
unknown <fixik2019@gmail.com>
2023-04-01 00:46:16 +0300

    save

---
+++ js/main.js
@@ -0,0 +1,18 @@
+function showPassword(){
+    const eye = document.querySelector('.password_img')
+    const first_input = document.querySelector('.input_password');
+    const second_input = document.querySelector('.input_confirm_password');
+
+    eye.addEventListener('click', () =>{
+        eye.classList.toggle('active');
+
+        if(first_input.getAttribute('type') === 'password' || second_input.getAttribute('type') === 'password'){
+            first_input.setAttribute('type', 'text')
+            second_input.setAttribute('type', 'text')
+        } else{
+            first_input.setAttribute('type', 'password')
+            second_input.setAttribute('type', 'password')
+        }
+    })
+}
+showPassword()
