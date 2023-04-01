a243eec5a367a7b3b897f5efa73c72047188af1f
unknown <fixik2019@gmail.com>
2023-04-02 01:03:58 +0300

    save

--- send.php
+++ send.php
@@ -1,9 +1,9 @@
 <?php
 if(!empty($_POST["send"])){
-    $login = $_POST["login"];
-    $password = $_POST["password"];
-    $email = $_POST["email"];
-    $message = $_POST["message"];
+    $login = htmlentities($_POST["login"]);
+    $password = htmlentities($_POST["password"]);
+    $email = htmlentities($_POST["email"]);
+    $message = htmlentities($_POST["message"]);
     $toEmail = "fixik2019@gmail.com";
 
     $mailHeaders = "Login: " . $login .
