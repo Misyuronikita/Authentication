c8f76ded6600ff9c75248d666c6ba38295a4f0d0
unknown <fixik2019@gmail.com>
2023-04-06 22:22:42 +0300

    save

--- send.php
+++ send.php
@@ -1,12 +1,19 @@
 <?php
-if(!empty($_POST["send"])){
+if($_SERVER["REQUEST_METHOD"] == "POST"){
     $login = htmlentities($_POST["login"]);
     $password = htmlentities($_POST["password"]);
     $email = htmlentities($_POST["email"]);
     $message = htmlentities($_POST["message"]);
     $toEmail = "fixik2019@gmail.com";
-
-    $mailHeaders = "Login: " . $login .
+    $subject = "My project message";
+    $body =  "Login: " . $login .
         "\r\n Password: " . $password .
         "\r\nMessage: " . $message . "\r\n";
+
+    if(mail($toEmail, $subject, $body)){
+        echo "Sending successfully";
+    }
+    else{
+        echo "ERROR: MESSAGE NOT SENT";
+    }
 }
